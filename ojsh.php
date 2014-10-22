c51a69674676a0646ebe6e3892d7ab998aeb6343
Brown, Bryan <bryjbrown@gmail.com>
2014-10-22 17:30:41 -0400

    Fixed encoding issues

--- ojsh.php
+++ ojsh.php
@@ -37,7 +37,10 @@ function get_clean_html($url) {
   $tidytext = tidy_parse_string($urltext);
   tidy_clean_repair($tidytext);
   $output = trim($tidytext);
-  $output = preg_replace('/\n/', '', $output);
+  $output = preg_replace('/\n/', ' ', $output);
+  $output = preg_replace('/>\s</', '><', $output);
+  $output = preg_replace('/=\s"/', '="', $output);
+  $output = html_entity_decode($output, ENT_COMPAT, 'ISO-8859-1');
   return $output;
 }
 
@@ -60,15 +63,39 @@ if (!$newest) {
 
 $tocurl = "$issueurl/showToc";
 $toctext = get_clean_html($tocurl);
+
 preg_match_all('/<table\ class="tocArticle".*?<\/table>/', $toctext, $tocresults);
 
-//echo $toctext;
-print_r($tocresults);
+foreach($tocresults[0] as $result) {
+  preg_match('/<td class="tocTitle">(<a href=".*?">)?(.*?)(<\/a>)?<\/td>/', $result, $titlearray);
+  $title = str_replace('"', '', $titlearray[2]);
+  preg_match('/<td class="tocAuthors">(.*?)<\/td>/', $result, $authorarray);
+  $author = $authorarray[1];
+  preg_match('/<td class="tocGalleys"><a href="(.*)" class="file">(.*)<\/a>/', $result, $filearray);
+  $fileurl = $filearray[1];
+  $filetype = $filearray[2];
 
-
+  echo "\n$title\n";
+  echo "$author\n";
+  echo "$filetype: $fileurl\n";
+  echo "-------------------------------------\n";
+}
 
 
 /*
+$ch = curl_init();
+$source = "http://someurl.com/afile.zip";
+curl_setopt($ch, CURLOPT_URL, $source);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$data = curl_exec ($ch);
+curl_close ($ch);
+
+$destination = "/asubfolder/afile.zip";
+$file = fopen($destination, "w+");
+fputs($file, $data);
+fclose($file);
+
+
 tocset = re.findall('<table class="tocArticle" width="100%">.*?</table>', tocdata)
   articlepattern = '<table class="tocArticle" width="100%"><tr valign="top"><td class="tocTitle">(<a href="(.*)">)?(.+?)(</a>)?</td><td class="tocGalleys"><a href="(.+?)" class="file">(.+?)</a></td></tr><tr><td class="tocAuthors">(.+?)</td><td class="tocPages"></td></tr></table>'
   articlepattern = '<table class="tocArticle" width="100%"><tr valign="top"><td class="tocTitle">(<a href="(.*)">)?(.+?)(</a>)?</td><td class="tocGalleys"><a href="(.+?)" class="file">PDF</a></td></tr><tr><td class="tocAuthors">(.+?)</td><td class="tocPages"></td></tr></table>'
